fbcf78c085f63fd38b2de49f0775f173146a7a14
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-03-11 12:36:58 +0300

    one more source has been added

--- async_launcher.py
+++ async_launcher.py
@@ -6,12 +6,12 @@ process of asynchronous web scraping.
 """
 import asyncio
 import aiohttp
-import test
+import data_manager
 from data_miner import DataFetcher as Df
 from scraping_info import books
 
 
-async def get_page(session, url):
+async def get_response(session, url):
 	"""
 	This function fetches the response data from the current page.
 	"""
@@ -35,16 +35,32 @@ async def collect_tasks_from_each_page(session):
 	for i in range(0, num_of_pages):
 		# get the task
 		n = i + 1
-		url = books['books-to-scrape']['source'].format(n)
-		task = asyncio.create_task(get_page(session, url))
+		task = asyncio.create_task(get_response(session, books['russian-books']['source'].format(n)))
+		print(books['russian-books']['source'].format(n))
 		tasks.append(task)
+
 	# gather all the tasks in one object corountine
 	result = await asyncio.gather(*tasks)
 
 	return result
 
 
-async def main():
+async def find_pages_amount(session):
+	"""
+	This async function invokes a parser method that finds an amount 
+	of pages and so defines a quantity of tasks to be performed.
+	"""
+
+	# get a single response
+	response = await get_response(session, books['russian-books']['source'].format(1))
+
+	# utilize a parser
+	amount = Df.get_pages_amount(response,'russian-books', books)
+
+	return amount
+
+
+async def begin_session():
 	"""
 	This function performs these two above,
 	giving a session object and initializing the web data.
@@ -58,39 +74,32 @@ async def main():
 		return data
 
 
-async def find_pages_amount(session):
+async def main():
 	"""
-	This async function invokes a parser method that finds an amount 
-	of pages and so defines a quantity of tasks to be performed.
+	This function gives a life to the other ones
+	and makes the tasks be performed in a loop.
 	"""
 
-	# get a single response
-	response = await get_page(session, books['books-to-scrape']['source'].format(1))
+	# take tasks
+	results = await begin_session()
 
-	# utilize a parser
-	amount = DataFetcher.get_pages_amount(response,'books-to-scrape', books)
+	# define a parsing object
+	scraper = Df(results, 'russian-books', books)
 
-	return amount
+	# get unstructured data from each task
+	content = scraper.fetch_content()
 
+	# get structured data
+	books_list = scraper.structure_data(content)
 
+	# write the gotten objects to the excel sheet
+	data_manager.write_objs_to_excel('tables//russian-books.xlsx', list_of_objs=books_list)
 
 
 if __name__ == '__main__':
 
-	# get the responses as tasks
-	results = asyncio.run(main())
-
-
-
-	# scrape the data in each task
-	# scraper = Df(results, 'books-to-scrape', books)
-
-	# get unstructured content
-	# content = scraper.fetch_content()
-
-	# get a list of structured objects
-	# books_list = scraper.structure_data(content)
-
-	# print(books_list)
-	# # dump all the objects to the excel table
-	# test.write_objs_to_excel('tables//books.xlsx', list_of_objs=books_list)
+	# define an event loop
+	loop = asyncio.get_event_loop()
+	
+	# start a machine
+	loop.run_until_complete(main())
--- scraping_info.py
+++ scraping_info.py
@@ -8,5 +8,16 @@ books = {
 		'image': {'tag': 'img', 'attribute': 'src'},
 		'generic_quantity': {'tag': 'form', 'class': 'form-horizontal', 'inlined_tag': 'strong'},
 		'obj_components': ['titles', 'integers', 'links', 'images']
+	},
+
+	'russian-books': {
+		'source': 'https://www.books.ru/bukhgalteriya-9000033/?page={}',
+		'object':  {'tag': 'div', 'class': 'book-catalog_item'},
+		'title': {'tag': 'a', 'class': 'custom-link book-catalog_item_title'},
+		'integer': {'tag': 'span', 'class': 'book-price'},
+		'link': {'tag': 'a'},
+		'image': {'tag': 'img', 'attribute': 'src'},
+		'generic_quantity': {'tag': 'span', 'class': 'catalog-category_value'},
+		'obj_components': ['titles', 'integers', 'links', 'images']
 	}
 }
