04ccdac4bf20e6fe8f826364be409c11352989ce
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-07-25 22:36:36 +0300

    minor integrations

---
+++ data_manager/data_dumping.py
@@ -0,0 +1,20 @@
+"""
+The tools for saving the scraped data to the database.
+
+"""
+
+
+class DataDumper:
+	"""
+	The object takes a list of
+	pythonic objects and dumps
+	them to the database.
+	"""
+	...
+
+
+class DataRemover:
+	"""
+	The object removes required data.
+	"""
+	...
