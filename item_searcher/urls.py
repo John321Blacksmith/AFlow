525b595a0df68fa3c177cbee997aac20d49fbd27
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-08-27 20:06:35 +0300

    refactored

--- item_searcher/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'item_searcher'
-urlpatterns = [
-	path('', views.search, name='search'),
-	path('results/', views.get_results, name='results'),
-]
-
--- item_searcher/views.py
+++ item_searcher/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
-def search(request):
-	return render(request, 'templates/search.html')
-
-
-def get_results(request):
-	return render(request, 'templates/results.html')
