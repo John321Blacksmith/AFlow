479f4af664aaf9098afe864a0a3378880848ad04
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-09-16 17:20:44 +0300

    LinksLoadingManager has been optimized

--- data_machine/managers/scraping_manager/engine.py
+++ data_machine/managers/scraping_manager/engine.py
@@ -1,18 +1,24 @@
+import sys
+import time
 import asyncio
-import pandas
+import pandas as pd
 import requests_html
 from urllib.parse import urljoin
 from requests_html import AsyncHTMLSession
-from scraping_info import scraping_data, auction_site
+
+sys.path.append('..\\..\\configs\\html_tags')
+from scraping_info import books
 
 
 class LinksLoadingManager:
     """
-    The object loads the links
-    from the website and saves
-    them to the flat file.
-    It also takes the saved links
-    from the file for crawling.
+    The object takes a list of
+    parsed links in order to 
+    save them all to the csv file
+    and, as the next step, pulls
+    them out from the file to use
+    each one in sending a request
+    the the server.
 
     async def save_links(source, file_name):
         :source: str
@@ -24,8 +30,8 @@ class LinksLoadingManager:
         :returns: list[str]
 
     """
-    def __init__(self, site_dict: dict[dict]):
-        self.site_dict = site_dict
+    def __init__(self, path_to_csv):
+        self.path_to_csv = path_to_csv
         
 
     async def save_links(self, parsed_links: list[str]) -> None:
@@ -34,9 +40,9 @@ class LinksLoadingManager:
         save each one to the disk.
         """
         try:
-            with open(self.site_dict['links_file'], mode='w') as f:
-                for link in parsed_links:
-                    f.write(link + '\n')
+            with open(self.path_to_csv, mode='w') as f:
+                df = pd.DataFrame(data=parsed_links)
+                df.to_csv(f, header=False, index=False, lineterminator='\n')
         except (Exception, FileNotFoundError) as error:
             print(f'Couldn\'t save data because {error}')
         else:
@@ -48,14 +54,30 @@ class LinksLoadingManager:
         stored and pull each one out to the list.
         """
         try:
-            with open(self.site_dict['links_file'], newline='') as f:
-                reader = csv.reader(f)
-                for i, row in enumerate(reader):
-                    links.append(row[0])
+            with open(self.path_to_csv, mode='r') as f:
+                links_frame = pd.read_csv(f, header=None)
+                for i in range(0, len(links_frame[0])):
+                    links.append(links_frame[0][i])
         except (Exception, FileNotFoundError) as error:
             print(f'Couldn\'t perform operation because {error}')
         else:
             return links
+
+    def csv_is_full(self) -> bool:
+        """
+        Check if the file has already
+        been stuffed with the links.
+        """
+        try:
+            with open(self.path_to_csv, mode='r') as f:
+                try:
+                    links_frame = pd.read_csv(f, header=None)
+                except pd.errors.EmptyDataError:
+                    return False
+                else:
+                    return True
+        except FileNotFoundError as error:
+            print('No such file or directory')
 
 
 class WebRequestManager:
@@ -156,7 +178,7 @@ class WebCrawler:
         else:
             return self.crawled_links
 
-    async def extract_links(self, url: str):
+    async def extract_links(self, url: str) -> list[str]:
         """
         Go through each page and
         parse its url recursively.
@@ -222,9 +244,37 @@ class DataFetcher:
 
 
 async def main():
-    webcrawler = WebCrawler(auction_site)
-    links = await webcrawler.extract_links(auction_site['source'])
-    print(links)
+    csv = '..\\..\\instant_files\\links.csv'
+    # classes instantiation
+    webcrawler = WebCrawler(site_dict=books)
+    links_manager = LinksLoadingManager(path_to_csv=csv)
+    # objects activities
+    # checking if the csv file is full
+    if links_manager.csv_is_full():
+        print('it is full.\nthe webcrawler is idle. Using this file...')
+        links = await links_manager.retrieve_links()
+    else:
+        print('it is empty, the webcrawler\'s gonna work...')
+        links = await webcrawler.extract_links(webcrawler.site_dict['source'])
+        await links_manager.save_links(parsed_links=links)
+
+
+    # throwing requests to the server & scraping the HTMLs
+    print('Parsing the data\n')
+    print('initializing the scraper...')
+    data_fetcher = DataFetcher(site_dict=books)
+    time.sleep(2)
+    objects = await data_fetcher.extract_data(urls=links)
+    print('extracting the objects...\n\n\n\n')
+    time.sleep(5)
+    print('THE RESULT IS BELOW\n')
+    try:
+        for i in range(0, len(objects)):
+            for key, value in objects[i].items():
+                print(key + ' ::: ' + str(value))
+            print('\n\n')
+    except IndexError:
+        print('No items found')
 
 
 if __name__ == '__main__':
