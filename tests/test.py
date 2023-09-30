241f8a5da288ed63314594bfd2a912a29fd016d2
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-09-30 21:10:47 +0300

    added tests for webscraper

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,17 @@
+import os
 import sys
-import pytest
-sys.path += [
-'..\\..\\data_machine\\configs\\html_tags',
-'..\\..\\data_machine\\managers'
-]
-from scraping_manager.engine import (
-									LinksLoadingManager,
-									DataFetcher
-								)
-from scraping_info import books
+import unittest
+from pathlib import Path
+ROOT_PATH = Path(__file__).resolve().parent
+sys.path.append(os.path.join(ROOT_PATH, ''))
 
+print(sys.path)
+
+
+from scraping_manager.engine import (
+		LinksLoadingManager,
+		DataFetcher
+	)
 
 class TestWebCrawlingLogic:
 	"""
@@ -31,8 +33,7 @@ class TestLinksLoaderLogic:
 	This test checks the work
 	of the LinksLoading Manager.
 	"""
-
-	def __init__(self):
+	def setUp(self):
 		self.mock_links = [
 			'https://www.test0.com',
 			'https://www.test1.com',
@@ -51,28 +52,32 @@ class TestLinksLoaderLogic:
 		Check if the links from the web
 		are saved to the CSV file well.
 		"""
-		result = self.links_manager.save_links(self.mock_links)
 		with open(self.file, 'r') as f:
-			assert len(f.readlines()) == len(self.mock_links)
-
-		assert result == True
+			self.assertEqual(len(f.readlines()), len(self.mock_links))
 
 	def test_the_manager_throws_false_if_links_not_cached(self):
 		"""
 		Check if the False
 		is thrown.
 		"""
-		assert self.unsuccessful_manager.save_links(self.wrong_file) == False
+		self.assertIs(self.unsuccessful_manager.save_links(self.wrong_file), False)
 
-	def test_the_cached_links_are_retrieved(self):
+	def test_the_cached_links_are_retrieved_successfully(self):
 		"""
 		Check if all the links are
 		taken from the HD and formed
 		to a python DS.
 		"""
 		retrieved_links = self.links_manager.retrieve_links()
-		assert len(retrieved_links) == len(self.mock_links)
-		assert retrieved_links == self.mock_links
+		self.assertEqual(len(retrieved_links), len(self.mock_links))
+		self.assertEqual(retrieved_links, self.mock_links)
+
+	def test_the_links_are_retrieved_unsuccessfully(self):
+		"""
+		Check if the links manager
+		throws the None.
+		"""
+		self.assertIs(self.unsuccessful_manager.retrieve_links(), None)
 
 	def test_the_filechecker_returns_false_if_empty_csv(self):
 		"""
@@ -80,9 +85,7 @@ class TestLinksLoaderLogic:
 		retrieves False when csv
 		is empty.
 		"""
-		with open(self.file, 'r') as f:
-
-		assert self.links_manager.csv_is_full() == False
+		self.assertIs(self.unsuccessful_manager.csv_is_full(), False)
 
 	def test_the_filechecker_returns_true_if_filled_csv(self):
 		"""
@@ -90,7 +93,7 @@ class TestLinksLoaderLogic:
 		retrieves False when csv
 		is empty.
 		"""
-		assert self.links_manager.csv_is_full() == True
+		self.assertIs(self.links_manager.csv_is_full(), True)
 
 
 class TestDataFetcherLogic:
