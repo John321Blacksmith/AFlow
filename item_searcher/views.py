fec24a31e23b93c0733225f3e173bc7808a2fc4b
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-10-01 20:30:16 +0300

    added the primitive web inteface

--- item_searcher/views.py
+++ item_searcher/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .import models
 
 # Create your views here.
 
+
+def index(request):
+	context = {'content': None}
+	return render(request, 'item_searcher/index.html', context=context)
